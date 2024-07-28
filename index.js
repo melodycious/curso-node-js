287760ad3371f8a0f6473f97386a35ba374e3749
melodycious <melody.callejas@gmail.com>
2024-07-28 22:31:15 +0200

    testing promesas

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ console.log('CPUs:', os.cpus())
 console.log('Memoria libre:', os.freemem() / 1024 / 1024)
 console.log('Memoria total:', os.totalmem() / 1024 / 1024) */
 
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 
 console.log('Leyendo el primer archivo...')
 
-const text = fs.readFile('./clase-1/archivo.txt', 'utf-8', (err, text) => {
-    console.log(text)
-})
+fs.readFile('./clase-1/archivo.txt', 'utf-8')
+    .then(text => {
+        console.log('primer texto:', text)
+    })
 
 console.log('haciendo cosas')
 
-const testingTest = fs.readFile('./clase-1/archivo2.txt', 'utf-8', (err, testingTest) => {
-    console.log(testingTest)
-})
+fs.readFile('./clase-1/archivo2.txt', 'utf-8')
+    .then (text => {
+        console.log('segundo texto:', text)
+    })
 
 
 
