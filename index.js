4c2b3a5954801a8956ed678b8564b83466a8d447
melodycious <melody.callejas@gmail.com>
2024-07-28 22:04:30 +0200

    pruebas info os

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!')
+const os = require('node:os')
+
+console.log('Informacion del sistema operativo:')
+console.log('--------------------------')
+console.log('Nombre del SO:', os.platform())
+console.log('Version del SO', os.release())
+console.log('Arquitectura', os.arch())
+console.log('CPUs:', os.cpus())
+console.log('Memoria libre:', os.freemem() / 1024 / 1024)
+console.log('Memoria total:', os.totalmem() / 1024 / 1024)
